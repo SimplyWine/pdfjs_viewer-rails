454d1ddfe1898cd589172726e6a7ae1a685b6ea4
roncito <ronald.reeser@gmail.com>
2018-09-24 14:30:21 -0400

    skip_after_action :intercom_rails_auto_include

--- app/controllers/pdfjs_viewer/viewer_controller.rb
+++ app/controllers/pdfjs_viewer/viewer_controller.rb
@@ -1,6 +1,7 @@
 module PdfjsViewer
   class ViewerController < ApplicationController
     layout false
+    skip_after_action :intercom_rails_auto_include
 
     def full
       response.headers.except! 'X-Frame-Options'
